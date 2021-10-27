602523dc6cd6cd6d317e9a6ca72b58195dc4992a
anastassiya.savostina <a.savostina@icloud.com>
2021-10-26 22:29:13 -0600

    Adding files

--- app.js
+++ app.js
@@ -5,18 +5,15 @@ var isHungry = true;
 var totalCost = pizzaCount * 8.00;
 
 //console.log, alert, document.write, target html element
-console.log( " Welcome " + myName  + " Pizzas cost $8 per each. " +
-"Your total is: $" + totalCost +
-" But I am still Hungry " + "That is" + " " + isHungry );
-alert( " Welcome " + myName  + " Pizzas cost $8 per each. " +
-"Your total is: $" + totalCost +
-" But I am still hungry! " +  "That is" + " " + isHungry );
-document.write( " Welcome " + myName  + " Pizzas cost $8 per each. " +
-"Your total is: $" + totalCost +
-" But I am still hungry! " + "That is" + " " + isHungry ); 
-document.getElementById("demo").innerHTML= " Welcome " + myName  + " Pizzas cost $8 each " +
-"Your total is: $" + totalCost +
-" But I am  still hungry! " + "That is" + " " + isHungry;
+
+alert("Welcome: " + myName);
+
+alert("Pizzas cost $8 per each");
+
+alert("Your total is: $" + totalCost);
+
+alert("Still Hungry: " + isHungry);
+
 
 
 
