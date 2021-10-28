d6e8415a644f2422a8c18977202ee64e685cd72f
anastassiya.savostina <a.savostina@icloud.com>
2021-10-28 17:38:05 -0600

    Adding files

--- app.js
+++ app.js
@@ -16,4 +16,22 @@ alert(" Still Hungry: " + isHungry );
 
 
 
+// Calculate properties of a circle
+// Store a radius into a variable
+var radius = 5;
+
+// Calculate the circumference based on the radius
+var circumference = Math.PI * 2*radius;
+
+//console.log
+console.log( " The circumference is " + circumference );
+
+// Calculate the area based on the radius
+var area = Math.PI * radius*radius;
+console.log( " The area is " + area );
+
+document.write( " The circumference is " + circumference );
+
+document.getElementById("demo").innerHTML=" The circumference is " + circumference;
+
 
